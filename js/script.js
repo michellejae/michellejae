60560995eaae8be7815e98d7e1d4d2318c2a97ac
michelle <moyer.mj@gmail.com>
2019-04-11 17:00:11 -0400

    name and margins

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let dropdownImg = document.createElement('img');
-dropdownImg.id = 'dropdownImg';
-dropdownImg.src = `/assets/dice.png`;
-dropdown.appendChild(dropdownImg)
+// let dropdownImg = document.createElement('img');
+// dropdownImg.id = 'dropdownImg';
+// dropdownImg.src = `/assets/dice.png`;
+// dropdown.appendChild(dropdownImg)
