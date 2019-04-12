fb11969c71011599b4069a36505fb882fe323e86
michelle <moyer.mj@gmail.com>
2019-04-12 18:20:34 -0400

    toggle dropdown

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-// let dropdownImg = document.createElement('img');
-// dropdownImg.id = 'dropdownImg';
-// dropdownImg.src = `/assets/dice.png`;
-// dropdown.appendChild(dropdownImg)
+function dropDown() {
+  document.getElementById("dropdown").classList.toggle("show")
+}
+
+window.onclick = function(e) {
+  if(!e.target.matches('.my-button')) {
+    let drop = document.getElementsByClassName("dropdown-content");
+    for(let i=0; i<drop.length; i++){
+      if(drop[i].classList.contains('show')) {
+        drop[i].classList.remove('show')
+      }
+    }
+   
+  }
+}
